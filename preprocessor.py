d879461c39fc21aaa50ced8f8a519b76f2928959
Neeraj Kumar Paikra <neeraj.naive.np@gmail.com>
2023-11-10 11:21:19 +0530

    Changes in preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -2,14 +2,15 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = '\d{1,2}\D\d{1,2}\D\d{2,4},\s\d{1,2}:\d{2}\s[APMapm]+\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
+    dates = [date.replace('\u202f', ' ') for date in dates]
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
     # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %I:%M %p - ')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
@@ -36,16 +37,5 @@ def preprocess(data):
     df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
-
-    period = []
-    for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
-
-    df['period'] = period
 
     return df
